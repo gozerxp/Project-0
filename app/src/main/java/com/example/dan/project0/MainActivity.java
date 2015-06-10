9e6bcf6c4a61a6fca5ed3a4186e81ae7ba9c33e3
Dan A <gozerxp@gmail.com>
2015-06-10 00:05:49 -0400

    4th commit, better handling of Toast

--- app/src/main/java/com/example/dan/project0/MainActivity.java
+++ app/src/main/java/com/example/dan/project0/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.dan.project0;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.content.Context;
 import android.view.View;
 import android.widget.Button;
@@ -17,27 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    private Toast mAppToast;
 
     public void displayToast(View view) {
         Button button = (Button)view;
@@ -45,7 +23,12 @@ public class MainActivity extends ActionBarActivity {
         Context context = getApplicationContext();
         CharSequence text = "You clicked " + buttonText + "!";
         int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+
+        if(mAppToast !=null){
+            mAppToast.cancel();
+        }
+
+        mAppToast = Toast.makeText(context,text, duration);
+        mAppToast.show();
     }
 }
